328663f1411b2f897f05c2f41113bfc4a329a27f
Henrique Tavella <henriquetavella0@gmail.com>
2026-08-11 13:19:14 -0300

    Adiciona estrutura básica da lista ligada

---
+++ C/EstruturasDinamicas/lista-ligada.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+// Estrutura que representa um elemento de uma Lista Ligada (ou "Linked List")
+struct Lista {
+    int valor;
+    struct Lista *prox;
+};
+struct Lista *inicio = NULL, *fim = NULL;
+
+int main() {
+}
