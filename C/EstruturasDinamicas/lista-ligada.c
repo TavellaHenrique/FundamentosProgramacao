1324e07c6f88539e125ccb558ac7ce97ed335bd6
Henrique Tavella <henriquetavella0@gmail.com>
2026-08-11 13:27:58 -0300

    Adiciona menu à lista ligada

--- C/EstruturasDinamicas/lista-ligada.c
+++ C/EstruturasDinamicas/lista-ligada.c
@@ -9,5 +9,38 @@ struct Lista {
 };
 struct Lista *inicio = NULL, *fim = NULL;
 
+void listar() {
+}
+
+void inserir() {
+}
+
+void remover() {
+}
+
 int main() {
+    setlocale(LC_ALL, "pt-br");
+
+    int op = 0;
+
+    while (op != 4) {
+        printf("\n\n|-- Lista Ligada --|");
+        printf("\n1. Listar");
+        printf("\n2. Inserir");
+        printf("\n3. Remover");
+        printf("\n4. Sair");
+
+        printf("\nOpção: ");
+        scanf("%d", &op);
+
+        switch (op) {
+            case 1: listar(); break;
+            case 2: inserir(); break;
+            case 3: remover(); break;
+            case 4: break;
+            default: printf("\nOpção inválida");
+        }
+    }
+
+    return 1;
 }
