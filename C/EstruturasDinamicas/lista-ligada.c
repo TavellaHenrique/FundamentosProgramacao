91071b4f283ca4e1c8ed9a221d4af3a413a3d744
Henrique Tavella <henriquetavella0@gmail.com>
2026-08-11 13:45:13 -0300

    Adiciona funções listar e inserir na lista ligada

--- C/EstruturasDinamicas/lista-ligada.c
+++ C/EstruturasDinamicas/lista-ligada.c
@@ -9,10 +9,45 @@ struct Lista {
 };
 struct Lista *inicio = NULL, *fim = NULL;
 
+// Lista o valor de cada elemento na lista
 void listar() {
+    if (inicio == NULL) {
+        printf("\nA lista está vazia.");
+        return;
+    }
+
+    for (struct Lista *elemento = inicio; elemento != NULL; elemento = elemento->prox) {
+        printf("\nValor: %d", elemento->valor);
+    }
 }
 
+// Insere um elemento ao final da lista
 void inserir() {
+    // Recebe valor do novo elemento
+    int valor = 0;
+    printf("\nDigite um número inteiro: ");
+    scanf("%d", &valor);
+
+    // Aloca memória para o novo elemento
+    struct Lista *elemento = (struct Lista *) malloc(sizeof(struct Lista));
+
+    // Define o valor do novo elemento
+    elemento->valor = valor;
+
+    // Como será inserido no final da lista, não tem próximo elemento
+    elemento->prox = NULL;
+
+    // Se a lista estiver vazia, este será o primeiro e último elemento
+    if (inicio == NULL) inicio = fim = elemento;
+    else {
+        // Se a lista possui ao menos um elemento:
+        // O atual último elemento aponta para o novo
+        fim->prox = elemento;
+        // O novo elemento se torna o fim da lista
+        fim = elemento;
+    }
+
+    printf("\nO elemento foi adicionado ao fim da lista.");
 }
 
 void remover() {
@@ -38,7 +73,7 @@ int main() {
             case 2: inserir(); break;
             case 3: remover(); break;
             case 4: break;
-            default: printf("\nOpção inválida");
+            default: printf("\nOpção inválida.");
         }
     }
 
